e0c90b6225e4f6661cf5abf3c1a8bb83038a0b93
wigscs <174610664+wigscs@users.noreply.github.com>
2024-08-15 14:04:58 +0100

    banner carousel

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.banner').slick({
+        adaptiveHeight: true,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3500,
+    });
+});
+            
