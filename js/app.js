db041ef0c5b7aec032bba9fc8619bc7f6cbe7a61
wigscs <174610664+wigscs@users.noreply.github.com>
2024-08-16 10:01:47 +0100

    Cookie consent popup

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Homepage banner carousel
 $(document).ready(function(){
     $('.banner').slick({
         adaptiveHeight: true,
@@ -7,4 +8,23 @@ $(document).ready(function(){
         autoplaySpeed: 3500,
     });
 });
-            
+
+/** 
+ * Cookie consent modal 
+ */
+const acceptCookies = document.getElementById('btn-accept-cookies');
+const manageCookies = document.getElementById('btn-manage-consent');
+const consentModal = document.querySelector('.cookie-consent');
+
+if (!localStorage.getItem("cookie-consent")) {
+    consentModal.style.display = '';
+}
+
+acceptCookies.addEventListener('click', function(e) {
+    localStorage.setItem("cookie-consent", true);
+    consentModal.style.display = consentModal.style.display === 'none' ? '' : 'none';
+});
+
+manageCookies.addEventListener('click', function(e) {
+    consentModal.style.display = consentModal.style.display === 'none' ? '' : 'none';
+});
